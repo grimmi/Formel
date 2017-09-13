9869cf6a0872c2dd13bbd99b5e1cae45ccba52a1
Arne Lehmann <arne.lehmann@gmx.de>
2017-09-13 20:42:05 +0200

    test project

---
+++ Formel.Tests/TransformerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Formel.Tests
+{
+    public class TransformerTests
+    {
+        [Fact]
+        public void Transform_ValidFormula_ReturnsTransformedOutput()
+        {
+            var input = "3 + 4";
+
+            var transformed = Formel.Transform(input);
+
+            Assert.Equal("3 4 +", transformed);
+        }
+    }
+}
